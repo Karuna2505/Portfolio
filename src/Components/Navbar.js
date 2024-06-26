52e60497b4176ef09547c23204287332ce55be85
Karuna2505 <karunamathur14@gmail.com>
2024-06-26 13:04:26 +0530

    ui update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,12 +5,11 @@ import icon from "../Assets/icon.png";
 const Navbar = () => {
 
   return (
-    <nav className="h-auto shadow-md z-10 flex justify-center items-center py-4 ">
-      <img src={icon} alt="icon" className="h-12 mt-3" />
-      <img src={icon} alt="icon" className="h-12 mt-3" />
+    <nav className="h-auto  z-10 flex justify-center items-center py-4 ">
+      <img src={icon} alt="icon" className="h-12 mt-3 cursor-pointer" />
+      <img src={icon} alt="icon" className="h-12 mt-3 cursor-pointer" />
       <div className="w-2/5 ml-52">
         <ul className="flex justify-evenly text-[#254336] font-semibold text-lg ">
-        <li><a href="#home" className='effect cursor-pointer'>Home</a></li>
         <li><a href="#about" className='effect cursor-pointer'>About</a></li>
         <li><a href="#skills" className='effect cursor-pointer'>Skills</a></li>
         <li><a href="#projects" className='effect cursor-pointer'>Projects</a></li>

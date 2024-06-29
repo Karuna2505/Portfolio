20bb1397fc76ec70718d753f582485bd19b2eef1
Karuna2505 <karunamathur14@gmail.com>
2024-06-29 21:16:34 +0530

    ScrollUp

--- src/Components/ScrollUp.js
+++ src/Components/ScrollUp.js
@@ -9,8 +9,8 @@ function ScrollUp() {
       };
   return (
     <div>
-      <button className='fixed bottom-0 right-0 m-10 h-12 w-12' onClick={scrollToTop}>
-        <img src='/scroll.png' alt="Scroll Up Button"/>
+      <button className='fixed bottom-0 right-0 m-10 h-12 w-12 ' onClick={scrollToTop}>
+        <img src='/scroll.png' alt="Scroll Up Button" />
       </button>
     </div>
   )

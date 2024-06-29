c16539c843e19b59c472db1444dbcc515b1e2766
Karuna2505 <karunamathur14@gmail.com>
2024-06-29 12:00:23 +0530

    Contact Page

---
+++ src/Components/contactdiv.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function contactdiv({title}) {
+  return (
+    <div className='flex flex-col items-start'>
+      <label className='text-2xl text-[#DAD3BE] font-semibold'>{title}</label>
+      <input placeholder={title} className='h-9 mt-4 mb-8 border-0 bg-inherit border-b-2 border-[#254336] text-black w-60 placeholder:text-black'/>
+    </div>
+  )
+}
+
+export default contactdiv

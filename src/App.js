39c4c9cab0b0b262d66d6a7fd8a7c1b03a93fd3d
Karuna2505 <karunamathur14@gmail.com>
2024-07-07 19:28:50 +0530

    Scroll Up Button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import Navbar from './Components/Navbar';
 import Home from './Pages/Home';
 import About from './Pages/About';
@@ -9,9 +9,26 @@ import Contact from './Pages/Contact';
 import ScrollUp from './Components/ScrollUp';
 import Links from './Components/Links';
 function App() {
+  const [isTop, setIsTop] = useState(true);
+
+  const checkScrollTop = () => {
+    if (window.pageYOffset === 0) {
+      setIsTop(true);
+    } else {
+      setIsTop(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', checkScrollTop);
+    return () => {
+      window.removeEventListener('scroll', checkScrollTop);
+    };
+  }, []);
   return (
     <div>
-      <ScrollUp />
+      {isTop ? <></>  :<ScrollUp />}
+      
       <Links />
       <Navbar />
       <Home />
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -8,7 +8,6 @@ function Project({title,image,description,github,link}) {
       <div className='text-2xl font-bold left-[83px] font-mono my-4 text-[#254336]'>{title}</div>
       <div className='flex'>
       <a href={github} target='_blank' rel='noreferrer'><img src="/github.png" alt="Github Link" className='h-7 w-7 mt-5 mx-2 '/></a>
-      <a href={link} target="_blank" rel="noreferrer"><img src="/sitelink.png" alt="Direct Link" className='h-7 w-7 mt-5 '/></a>
       </div>
       </div>
       <span className='text-base text-[#DAD3BE]'>{description}</span>

abc2c922adaeaf121ec7b2b66c21a9e66a825f1e
Karuna2505 <karunamathur14@gmail.com>
2024-07-21 18:28:01 +0530

    Added Project

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -6,6 +6,7 @@ const projectsData=[
   {id:2,title:'News App',image:"/News.jpg",description:"Developed a news app integrating Firebase for secure user authentication,Firestore to efficiently store user preferences such as liked articles, and NewsAPI for seamless real-time data fetching. Demonstrated proficiency in leveraging diverse technologies to enhance app functionality and user experience.",github:"https://github.com/Karuna2505/news-reader",link:"https://phenomenal-nasturtium-8ff1f4.netlify.app/"},
   {id:3,title:"Weather App",image:"/weather.jpg",description:"This website shows real-time and forecast weather data in a user-friendly and responsive UI using Weather API. You can also search for any city worldwide and get weather info quickly. Search also has an autocomplete feature.",github:"https://github.com/Karuna2505/Weather-App",link:"https://weather-app-bykaruna.netlify.app/"},
   {id:4,title:'ToDo App',image:"/todo.jpg",description:"Developed this website, to allow users to add and manage their todo-items.You can delete the item when you want to.",github:"https://github.com/Karuna2505/Todo-App",link:"https://todoappbykaruna.netlify.app/"},
+  {id:5,title:"Verdant",image:"/Verdant.jpg",description:"Developed a full stack plant selling website.Created an api from where the data is bieng fetched.Implemented user authentiaction.Created reponsive UI for better experience.",github:"https://github.com/Karuna2505/Verdant",link:"https://verdant-frontend.onrender.com/"},
   
 ]
 
